0b2f08fcae14e75d1afdea5a550f25fbf1b5ad94
DreasWeiss <dreasweiss@gmail.com>
2023-03-10 21:20:34 +0300

    mongodb connect

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 
-
-
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -15,7 +15,22 @@ app.get('/', (req, res) => {
 
 
 
+const mongoDbUri = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPAS}@freecodecampexercisetra.p4prxcl.mongodb.net/exerciseTracker`;
+const connectDb = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    const conn = await mongoose.connect(mongoDbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
 
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+connectDb();
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
