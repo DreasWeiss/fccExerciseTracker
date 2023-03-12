a6572ba6d806a30da87327495a0b3f37a05ce485
DreasWeiss <dreasweiss@gmail.com>
2023-03-12 20:29:38 +0300

    exercises post add + exersices schema + model

--- README.md
+++ README.md
@@ -19,8 +19,8 @@
 - [ ] You can make a GET request to /api/users to get a list of all users.
 - [ ] The GET request to /api/users returns an array.
 - [ ] Each element in the array returned from GET /api/users is an object literal containing a user's username and _id.
-- [ ] You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
-- [ ] The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+- [x] You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+- [x] The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
 - [ ] You can make a GET request to /api/users/:_id/logs to retrieve a full exercise log of any user.
 - [ ] A request to a user's log GET /api/users/:_id/logs returns a user object with a count property representing the number of exercises that belong to that user.
 - [ ] A GET request to /api/users/:_id/logs will return the user object with a log array of all the exercises added.
--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(express.static('public'));
 const personSchema = new Schema({ username: String });
 const Person = mongoose.model('Person', personSchema);
 
+const exerciseSchema = new Schema({
+  '_id': {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: Date
+});
+const Exersice = mongoose.model('Exersice', exerciseSchema);
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
@@ -25,6 +42,50 @@ app.post('/api/users', async (req, res) => {
     console.log(err);
   }
 });
+
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  try {
+    const id = req.params._id;
+    const { description, duration, date } = req.body;
+    await Person.findById(id, (err, personData) => {
+      if (err || !personData) {
+        res.send(`Could not find any user(person) with id ${id}`);
+      } else {
+        const newExercise = new Exersice({
+          '_id': id,
+          description,
+          duration,
+          date: new Date()
+        });
+        newExercise.save((err, data) => {
+          if (err || !data) {
+            res.send('There was an error saving this exercise');
+          } else {
+            const { description, duration, date, _id } = data;
+            res.json({
+              username: personData.username,
+              description,
+              duration,
+              date: date.toDateString(),
+              _id: personData.id
+            })
+          }
+        })
+      }
+    });
+    // const newExercise = new Exersice({ _id, description, duration, date });
+    // await newExercise.save();
+    // res.json({
+    //   '_id': newExercise.userId,
+    //   'description': newExercise.description,
+    //   'duration': newExercise.duration,
+    //   'date': newExercise.date
+    // });
+
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 
 
